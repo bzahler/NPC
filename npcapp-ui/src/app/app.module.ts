c6f6c827e16cf2ad2b2c706b55e3b621e205e209
bzahler <maaraken@yahoo.com>
2019-01-07 23:43:35 -0500

    add angular material stuff. Added a lot of modules to app.module.ts. Got routing sorted out. I do believe the app is ready to be developed.

--- npcapp-ui/src/app/app.module.ts
+++ npcapp-ui/src/app/app.module.ts
@@ -2,17 +2,59 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { 
+  MatToolbarModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatPaginatorModule, MatTableModule,
+  MatFormFieldModule, MatSortModule, MatIconModule, MatDialogModule, MatButtonModule, MatGridListModule,
+  MatNativeDateModule, MatDatepickerModule, MatCheckboxModule, MatSidenavModule, MatChipsModule, MatListModule, MatExpansionModule,
+  MatDividerModule, MatMenuModule, MatTabsModule, MatProgressBarModule, MatSnackBarModule, MatButtonToggleModule, MatRadioModule,
+  MatOptionModule, MatSelectModule, MatTooltipModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { NpcComponent } from './components/npc/npc.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     NpcComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    MatToolbarModule,
+    MatCardModule, 
+    MatAutocompleteModule, 
+    MatInputModule, 
+    MatPaginatorModule, 
+    MatTableModule,
+    MatFormFieldModule, 
+    MatSortModule, 
+    MatIconModule, 
+    MatDialogModule, 
+    MatButtonModule, 
+    MatGridListModule,
+    MatNativeDateModule, 
+    MatDatepickerModule, 
+    MatCheckboxModule, 
+    MatSidenavModule, 
+    MatChipsModule, 
+    MatListModule, 
+    MatExpansionModule,
+    MatDividerModule, 
+    MatMenuModule, 
+    MatTabsModule, 
+    MatProgressBarModule, 
+    MatSnackBarModule, 
+    MatButtonToggleModule, 
+    MatRadioModule,
+    MatOptionModule, 
+    MatSelectModule, 
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ npcapp-ui/src/app/routes.ts
@@ -0,0 +1,9 @@
+import { NpcComponent } from './components/npc/npc.component';
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+    {
+        path: 'npc',
+        component: NpcComponent,
+    }
+]
