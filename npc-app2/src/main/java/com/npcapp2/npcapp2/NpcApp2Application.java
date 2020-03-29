89e9299f02b26b0bac31910a9d484b61309611e9
bzahler <maaraken@yahoo.com>
2020-03-28 23:26:55 -0400

    created version 2 of the project for use with mongodb and updated software

---
+++ npc-app2/src/main/java/com/npcapp2/npcapp2/NpcApp2Application.java
@@ -0,0 +1,13 @@
+package com.npcapp2.npcapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpcApp2Application.class, args);
+	}
+
+}
---
+++ npc-app2/src/test/java/com/npcapp2/npcapp2/NpcApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npcapp2.npcapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
