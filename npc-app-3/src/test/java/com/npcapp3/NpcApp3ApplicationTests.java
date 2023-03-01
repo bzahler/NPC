1dceddecd05aeae8e837dfeabac1236b88f61baf
bzahler <maaraken@yahoo.com>
2023-02-28 22:32:15 -0600

    Onwards to round 3 of the npc app!

---
+++ npc-app-3/src/test/java/com/npcapp3/NpcApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npcapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
